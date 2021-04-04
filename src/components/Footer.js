018900d3c49ca75596c9d72cf28d275b9a6bad24
Gram Liu <gram.liu226@gmail.com>
2021-04-04 13:37:32 -0400

    Added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,35 @@
+import {
+  Button,
+  Confirm,
+  Grid,
+  Icon,
+  Label,
+  Popup,
+  Segment,
+} from "semantic-ui-react"
+
+const footerStyle = {
+  position: "absolute",
+  bottom: 0,
+  width: "100%",
+  textAlign: "center"
+}
+
+const footerTextStyle = {
+  color: "white"
+}
+
+const Footer = (props) => {
+  return (
+    <div style={{ position: "relative", marginTop: "1em" }}>
+      <Segment style={footerStyle}>
+        <p style={footerTextStyle}>
+          Developed with ❤️ by 
+          <a href="https://scottylabs.org/" target="_blank"> ScottyLabs</a>
+        </p>
+      </Segment>
+    </div>
+  )
+}
+
+export default Footer
