2293e9797d6bf3be9f33bdc18106be5af9cea0a8
Jason <27450071+jyao6429@users.noreply.github.com>
2021-10-23 19:27:28 -0400

    Create initial gallery page and routes

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import "semantic-ui-css/semantic.min.css"
 import "./App.css"
 import Editor from "./components/Editor"
 import Navbar from "./components/Navbar"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import Gallery from "./components/Gallery"
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom"
 
 function App() {
   return (
     <div>
       <Router>
+        <Navbar />
         <Switch>
-          <Route path="/">
-            <Navbar />
+          <Route exact path="/">
             <Editor />
           </Route>
+          <Route path="/gallery">
+            <Gallery />
+          </Route> 
+          <Redirect to="/"/>
         </Switch>
       </Router>
     </div>
